df61f9349ab77a2efe5faa17ee1682cbfab178b2
kyeong-hyeok <minkh4227@naver.com>
2023-07-23 21:32:49 +0900

    feat: 프로젝트 생성 (#1)

---
+++ src/main/java/com/project/mapdagu/MapdaguApplication.java
@@ -0,0 +1,13 @@
+package com.project.mapdagu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapdaguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapdaguApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mapdagu/MapdaguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mapdagu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapdaguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
