85135bb03508ad18dcb276968f1bcb03388f1bec
kyeong-hyeok <minkh4227@naver.com>
2023-07-26 17:18:46 +0900

    feat: JPA Auditing 어노테이션 적용 (#3)

--- src/main/java/com/project/mapdagu/MapdaguApplication.java
+++ src/main/java/com/project/mapdagu/MapdaguApplication.java
@@ -2,7 +2,9 @@ package com.project.mapdagu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MapdaguApplication {
 
