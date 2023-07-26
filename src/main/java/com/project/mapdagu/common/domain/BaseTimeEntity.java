34d0d4537034a1978ed637575493fdadba14990b
kyeong-hyeok <minkh4227@naver.com>
2023-07-26 17:17:05 +0900

    feat: BaseTimeEntity 생성 (#3)

---
+++ src/main/java/com/project/mapdagu/common/domain/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.project.mapdagu.common.domain;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EnableJpaAuditing
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
