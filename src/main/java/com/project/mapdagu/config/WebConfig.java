32b8cca0b59d0327238ee40b021fbb4f98c12766
kyeong-hyeok <minkh4227@naver.com>
2023-07-29 00:07:54 +0900

    feat: CORS 설정

---
+++ src/main/java/com/project/mapdagu/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.project.mapdagu.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .maxAge(3000);
+
+    }
+}
