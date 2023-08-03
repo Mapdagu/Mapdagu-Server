184521ce690dfa0adc6f3179193a03c388f17e75
kyeong-hyeok <minkh4227@naver.com>
2023-08-03 16:40:44 +0900

    feat: JWT Test API 추가 (#4)

--- src/main/java/com/project/mapdagu/domain/auth/controller/AuthController.java
+++ src/main/java/com/project/mapdagu/domain/auth/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.project.mapdagu.domain.member.dto.request.SignUpRequestDto;
 import com.project.mapdagu.domain.member.dto.response.SignUpResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +17,14 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/api/sign-up")
+    @PostMapping("/sign-up")
     public ResponseEntity<SignUpResponseDto> signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
         SignUpResponseDto signUpResponseDto = authService.signUp(signUpRequestDto);
         return ResponseDto.created(signUpResponseDto);
+    }
+
+    @GetMapping("/jwt-test")
+    public String jwtTest() {
+        return "jwtTest 요청 성공";
     }
 }
