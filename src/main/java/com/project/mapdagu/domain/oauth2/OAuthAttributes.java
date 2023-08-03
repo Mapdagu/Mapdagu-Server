7c0451c76d7cbfd286f31cfddfc121bd9d2f9d88
kyeong-hyeok <minkh4227@naver.com>
2023-08-03 16:37:18 +0900

    feat: OAuth 2.0 관련 클래스 생성 (#4)

---
+++ src/main/java/com/project/mapdagu/domain/oauth2/OAuthAttributes.java
@@ -0,0 +1,69 @@
+package com.project.mapdagu.domain.oauth2;
+
+import com.project.mapdagu.domain.member.entity.Member;
+import com.project.mapdagu.domain.member.entity.Role;
+import com.project.mapdagu.domain.member.entity.SocialType;
+import com.project.mapdagu.domain.oauth2.userinfo.GoogleOAuth2UserInfo;
+import com.project.mapdagu.domain.oauth2.userinfo.KakaoOAuth2UserInfo;
+import com.project.mapdagu.domain.oauth2.userinfo.NaverOAuth2UserInfo;
+import com.project.mapdagu.domain.oauth2.userinfo.OAuth2UserInfo;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Getter
+public class OAuthAttributes {
+
+    private String nameAttributeKey; // OAuth2 로그인 진행 시 키가 되는 필드 값, PK
+    private OAuth2UserInfo oauth2UserInfo; // 소셜 타입별 로그인 유저 정보
+
+    @Builder
+    public OAuthAttributes(String nameAttributeKey, OAuth2UserInfo oauth2UserInfo) {
+        this.nameAttributeKey = nameAttributeKey;
+        this.oauth2UserInfo = oauth2UserInfo;
+    }
+
+    public static OAuthAttributes of(SocialType socialType, String userNameAttributeName, Map<String, Object> attributes) {
+
+        if (socialType == SocialType.NAVER) {
+            return ofNaver(userNameAttributeName, attributes);
+        }
+        if (socialType == SocialType.KAKAO) {
+            return ofKakao(userNameAttributeName, attributes);
+        }
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .nameAttributeKey(userNameAttributeName)
+                .oauth2UserInfo(new KakaoOAuth2UserInfo(attributes))
+                .build();
+    }
+
+    public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .nameAttributeKey(userNameAttributeName)
+                .oauth2UserInfo(new GoogleOAuth2UserInfo(attributes))
+                .build();
+    }
+
+    public static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .nameAttributeKey(userNameAttributeName)
+                .oauth2UserInfo(new NaverOAuth2UserInfo(attributes))
+                .build();
+    }
+
+    public Member toEntity(SocialType socialType, OAuth2UserInfo oauth2UserInfo) {
+        return Member.builder()
+                .socialType(socialType)
+                .socialId(oauth2UserInfo.getId())
+                .email(UUID.randomUUID() + "@socialUser.com")
+                .nickname(oauth2UserInfo.getNickname())
+                .role(Role.GUEST)
+                .build();
+    }
+}
