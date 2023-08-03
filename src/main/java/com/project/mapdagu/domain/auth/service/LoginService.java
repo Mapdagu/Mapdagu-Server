6310426fccc1b8b8b5409e3d23fdb4fd86afd957
kyeong-hyeok <minkh4227@naver.com>
2023-08-03 12:52:48 +0900

    feat: LoginService와 로그인 성공, 실패 핸들러 추가 (#4)

---
+++ src/main/java/com/project/mapdagu/domain/auth/service/LoginService.java
@@ -0,0 +1,31 @@
+package com.project.mapdagu.domain.auth.service;
+
+import com.project.mapdagu.domain.member.entity.Member;
+import com.project.mapdagu.domain.member.repository.MemberRepository;
+import com.project.mapdagu.error.ErrorCode;
+import com.project.mapdagu.error.exception.custom.BusinessException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static com.project.mapdagu.error.ErrorCode.*;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email).orElseThrow(() -> new BusinessException(MEMBER_NOT_FOUND));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().name())
+                .build();
+    }
+}
