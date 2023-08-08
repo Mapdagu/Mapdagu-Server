6336bbe71674eab3fac17005d94bb79f3d9fcedb
kyeong-hyeok <minkh4227@naver.com>
2023-08-09 01:36:55 +0900

    fix: CreatedDate, ModifiedDate 설정 안 되는 오류 해결

--- src/main/java/com/project/mapdagu/common/entity/BaseTimeEntity.java
+++ src/main/java/com/project/mapdagu/common/entity/BaseTimeEntity.java
@@ -1,16 +1,18 @@
 package com.project.mapdagu.common.entity;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
-@EnableJpaAuditing
+@EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
 
     @CreatedDate
