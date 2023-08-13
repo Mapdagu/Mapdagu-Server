17f37b2ebfe39a6310e5f13d1993de0c2ef2df84
kyeong-hyeok <minkh4227@naver.com>
2023-08-13 21:59:33 +0900

    refactor: Jwt 인증 필터 리팩토링 (#14)

--- src/main/java/com/project/mapdagu/jwt/filter/JwtAuthenticationProcessingFilter.java
+++ src/main/java/com/project/mapdagu/jwt/filter/JwtAuthenticationProcessingFilter.java
@@ -19,12 +19,15 @@ import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMap
 import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+
+import static com.project.mapdagu.error.ErrorCode.ALREADY_LOGOUT_MEMBER;
 
 /**
  * Jwt 인증 필터
@@ -34,7 +37,7 @@ import java.util.Set;
 @Slf4j
 public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
 
-    private static final Set<String> NO_CHECK_URLS = new HashSet<>(Arrays.asList("/login", "/auth/logout")); // "/login"으로 들어오는 요청은 Filter 작동 X
+    private static final Set<String> NO_CHECK_URLS = new HashSet<>(Arrays.asList("/login", "/api/auth/logout")); // "/login"으로 들어오는 요청은 Filter 작동 X
 
     private final JwtService jwtService;
     private final MemberRepository memberRepository;
@@ -46,48 +49,61 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
      * JWT 인증 필터
      */
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         if (NO_CHECK_URLS.stream().anyMatch(url -> url.equals(request.getRequestURI()))) {
             filterChain.doFilter(request, response); // NO_CHECK_URLS 요청이 들어오면, 다음 필터 호출
             return;
         }
         log.info("uri = {}, query = {}", request.getRequestURI(), request.getQueryString());
-
         log.info("JwtAuthenticationProcessingFilter 호출");
-        // 사용자 요청 헤더에서 RefreshToken 추출-> RefreshToken이 없거나 유효하지 않다면 null
-        String refreshToken = jwtService.extractRefreshToken(request)
-                .filter(jwtService::isTokenValid)
-                .orElse(null);
 
-        // 리프레시 토큰이 요청 헤더에 존재하고 유효하다면, AccessToken이 만료된 것 -> AccessToken/RefreshToken 재발급
-        if (refreshToken != null) {
-            log.info("RefreshToken 요청 헤더에 존재");
-            String email = jwtService.extractEmail(refreshToken).orElseThrow(() -> new TokenException(ErrorCode.INVALID_TOKEN));
-            if (isRefreshTokenMatch(email, refreshToken)) {
-                log.info("RefreshToken 유효, AccessToken 만료");
-                reIssueRefreshAndAccessToken(response, refreshToken, email);
+        String accessToken = jwtService.extractAccessToken(request).orElse(null);
+        if (jwtService.isTokenValid(accessToken)) {
+            if(redisUtil.hasKeyBlackList(accessToken)) {
+                throw new TokenException(ALREADY_LOGOUT_MEMBER);
             }
+            getAuthentication(accessToken);
+            filterChain.doFilter(request, response);
             return;
         }
 
-        // RefreshToken이 없다면 AccessToken을 검사하고 인증 처리
-        // AccessToken이 없거나 유효하지 않다면, 인증 객체가 담기지 않은 상태로 다음 필터로 넘어가기 때문에 403 에러 발생
-        // AccessToken이 유효하다면, 인증 객체가 담긴 상태로 다음 필터로 넘어가기 때문에 인증 성공
-        else {
-            checkAccessTokenAndAuthentication(request, response, filterChain);
+        // Access Token이 만료되어 Refresh Token으로 Access Token 재발급
+        String refreshToken = jwtService.extractRefreshToken(request).orElse(null);
+        if (StringUtils.hasText(accessToken) && jwtService.isTokenValid(refreshToken)) {
+            log.info("AccessToken 재발급");
+            String email = jwtService.extractEmail(refreshToken).orElseThrow(() -> new TokenException(ErrorCode.INVALID_TOKEN));
+            if (isRefreshTokenMatch(email, refreshToken)) {
+                reIssueRefreshAndAccessToken(response, refreshToken, email);
+            }
+            filterChain.doFilter(request, response);
+            return;
         }
+
+        log.info("유효한 JWT 토큰이 없습니다. uri: {}, {}", request.getRequestURI(), accessToken);
+        filterChain.doFilter(request, response);
     }
 
     /**
-     * AccessToken, RefreshToken 재발급 메소드 + 응답 헤더에 보내기
+     * AccessToken, RefreshToken 재발급 + 인증 + 응답 헤더에 보내기
      */
     private void reIssueRefreshAndAccessToken(HttpServletResponse response, String refreshToken, String email) {
         String newAccessToken = jwtService.createAccessToken(email);
         String newRefreshToken = jwtService.createRefreshToken(email);
+        getAuthentication(newAccessToken);
         redisUtil.delete(email);
         jwtService.updateRefreshToken(email, newRefreshToken);
         jwtService.sendAccessAndRefreshToken(response, newAccessToken, refreshToken);
         log.info("AccessToken, RefreshToken 재발급 완료");
+    }
+
+    /**
+     * AccessToken 재발급 + 인증 메소드 + 응답 헤더에 보내기
+     */
+    private void reIssueAccessToken(HttpServletResponse response, String refreshToken, String email) {
+        String newAccessToken = jwtService.createAccessToken(email);
+        jwtService.sendAccessAndRefreshToken(response, newAccessToken, refreshToken);
+        getAuthentication(newAccessToken);
+        log.info("AccessToken 인증 완료");
     }
 
     /**
@@ -102,19 +118,14 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
     }
 
     /**
-     * [액세스 토큰 체크 & 인증 처리 메소드]
+     * [인증 처리 메소드]
      * 인증 허가 처리된 객체를 SecurityContextHolder에 담기
      */
-    public void checkAccessTokenAndAuthentication(HttpServletRequest request, HttpServletResponse response,
-                                                  FilterChain filterChain) throws ServletException, IOException {
-        log.info("액세스 토큰 체크 & 인증 처리 메소드 checkAccessTokenAndAuthentication() 호출");
-        jwtService.extractAccessToken(request)
-                .filter(jwtService::isTokenValid)
-                .ifPresent(accessToken -> jwtService.extractEmail(accessToken)
-                        .ifPresent(email -> memberRepository.findByEmail(email)
-                                .ifPresent(this::saveAuthentication)));
-
-        filterChain.doFilter(request, response);
+    public void getAuthentication(String accessToken) {
+        log.info("인증 처리 메소드 getAuthentication() 호출");
+        jwtService.extractEmail(accessToken)
+                .ifPresent(email -> memberRepository.findByEmail(email)
+                        .ifPresent(this::saveAuthentication));
     }
 
     /**
