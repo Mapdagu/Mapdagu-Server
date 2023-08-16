a16256dc308d15d6d251c070acf0285800c0563b
kyeong-hyeok <minkh4227@naver.com>
2023-08-16 15:34:09 +0900

    feat: 테스트 평가 저장에 필요한 엔티티 추가 (#19)

---
+++ src/main/java/com/project/mapdagu/domain/evaluation/entity/Evaluation.java
@@ -0,0 +1,29 @@
+package com.project.mapdagu.domain.evaluation.entity;
+
+import com.project.mapdagu.domain.food.entity.Food;
+import com.project.mapdagu.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+public class Evaluation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memberId")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "foodId")
+    private Food food;
+
+    private Integer score;
+}
---
+++ src/main/java/com/project/mapdagu/domain/food/entity/Food.java
@@ -0,0 +1,26 @@
+package com.project.mapdagu.domain.food.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer imageNum;
+
+    private Integer scoville;
+}
