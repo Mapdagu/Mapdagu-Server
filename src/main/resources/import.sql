6ced0e5b1e875d3d5152c9cb817f8fb416836caf
kyeong-hyeok <minkh4227@naver.com>
2023-08-16 18:01:34 +0900

    chore: import.sql 수정

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,8 +2,8 @@
 INSERT INTO Member (id, nickname, email, userName, imageNum, intro, scoville, level, socialType, socialId, createdDate, modifiedDate) VALUES (1, '민경혁', 'minkh@naver.com', '경혁', 2, '안녕하세요', 3500, 5, null, null, now(), now());
 INSERT INTO Member (id, nickname, email, userName, imageNum, intro, scoville, level, socialType, socialId, createdDate, modifiedDate) VALUES (2, '김서린', 'seorin@naver.com', '서린', 3, '안녕하세용', 4000, 6, null, null, now(), now());
 
-INSERT INTO FOOD (id, name, imageNum, scoville) VALUES (1, '신라면', 1, 3400);
-INSERT INTO FOOD (id, name, imageNum, scoville) VALUES (2, '진라면', 2, 1270);
-INSERT INTO FOOD (id, name, imageNum, scoville) VALUES (3, '너구리', 3, 2300);
-INSERT INTO FOOD (id, name, imageNum, scoville) VALUES (4, '불닭볶음면', 4, 4404);
-INSERT INTO FOOD (id, name, imageNum, scoville) VALUES (5, '삼양라면', 5, 3000);
+INSERT INTO Food (id, name, imageNum, scoville) VALUES (1, '신라면', 1, 3400);
+INSERT INTO Food (id, name, imageNum, scoville) VALUES (2, '진라면', 2, 1270);
+INSERT INTO Food (id, name, imageNum, scoville) VALUES (3, '너구리', 3, 2300);
+INSERT INTO Food (id, name, imageNum, scoville) VALUES (4, '불닭볶음면', 4, 4404);
+INSERT INTO Food (id, name, imageNum, scoville) VALUES (5, '삼양라면', 5, 3000);
