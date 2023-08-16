1e013802902e63e4b97331e29ef070f1e7831eb9
kyeong-hyeok <minkh4227@naver.com>
2023-08-16 17:45:37 +0900

    chore: application-local.yml 설정 및 data.sql 설정

--- src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO FOOD (id, name, imageNum, scoville) VALUES (1, '신라면', 1, 3400);
-INSERT INTO FOOD (id, name, imageNum, scoville) VALUES (2, '진라면', 2, 1270);
-INSERT INTO FOOD (id, name, imageNum, scoville) VALUES (3, '너구리', 3, 2300);
-INSERT INTO FOOD (id, name, imageNum, scoville) VALUES (4, '불닭볶음면', 4, 4404);
-INSERT INTO FOOD (id, name, imageNum, scoville) VALUES (5, '삼양라면', 5, 3000);
