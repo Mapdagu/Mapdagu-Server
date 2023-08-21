7380c619a64557a92ba31889101ef0e559005d76
kyeong-hyeok <minkh4227@naver.com>
2023-08-21 21:16:41 +0900

    test: AuthController 테스트 코드 추가 (#22)

---
+++ src/test/java/com/project/mapdagu/domain/auth/controller/AuthControllerTest.java
@@ -0,0 +1,54 @@
+package com.project.mapdagu.domain.auth.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.mapdagu.domain.auth.service.AuthService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WithMockUser
+@ExtendWith(MockitoExtension.class)
+class AuthControllerTest {
+
+    @InjectMocks
+    private AuthController authController;
+    @Mock
+    private AuthService authService;
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(authController)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .build();
+    }
+
+    @Test
+    void 로그아웃() throws Exception {
+        //given
+        //when
+        ResultActions result = mockMvc.perform(
+                post("/api/auth/logout")
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        //then
+        result.andExpect(status().isNoContent());
+        verify(authService, times(1)).logout(any());
+    }
+}
