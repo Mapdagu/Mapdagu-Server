f759958a373e5c8f724e9a8112a75d8c47953b5a
kyeong-hyeok <minkh4227@naver.com>
2023-08-23 18:15:30 +0900

    feat: 맵기 평가 조회 시 imageNum 반환하도록 추가 (#24)

--- src/main/java/com/project/mapdagu/domain/evaluation/dto/response/EvaluationGetResponseDto.java
+++ src/main/java/com/project/mapdagu/domain/evaluation/dto/response/EvaluationGetResponseDto.java
@@ -2,9 +2,9 @@ package com.project.mapdagu.domain.evaluation.dto.response;
 
 import com.project.mapdagu.domain.evaluation.entity.Evaluation;
 
-public record EvaluationGetResponseDto(String name, Integer score) {
+public record EvaluationGetResponseDto(String name, Integer imageNum, Integer score) {
 
     public static EvaluationGetResponseDto from(Evaluation evaluation) {
-        return new EvaluationGetResponseDto(evaluation.getFood().getName(), evaluation.getScore());
+        return new EvaluationGetResponseDto(evaluation.getFood().getName(), evaluation.getFood().getImageNum(), evaluation.getScore());
     }
 }
