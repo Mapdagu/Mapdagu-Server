40481af9ef5fba8a025e13dcc4f99e1631208934
kyeong-hyeok <minkh4227@naver.com>
2023-08-25 11:07:57 +0900

    test: 맵기 평가 수정 Controller 테스트 코드 추가 (#27)

--- src/test/java/com/project/mapdagu/domain/evaluation/controller/EvaluationControllerTest.java
+++ src/test/java/com/project/mapdagu/domain/evaluation/controller/EvaluationControllerTest.java
@@ -1,9 +1,12 @@
 package com.project.mapdagu.domain.evaluation.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.mapdagu.common.dto.PageResponseDto;
 import com.project.mapdagu.domain.evaluation.dto.request.EvaluationInfoRequestDto;
 import com.project.mapdagu.domain.evaluation.dto.request.EvaluationSaveRequestDto;
+import com.project.mapdagu.domain.evaluation.dto.request.EvaluationUpdateRequestDto;
 import com.project.mapdagu.domain.evaluation.dto.response.EvaluationGetResponseDto;
+import com.project.mapdagu.domain.evaluation.dto.response.EvaluationsGetResponseDto;
 import com.project.mapdagu.domain.evaluation.service.EvaluationService;
 import com.project.mapdagu.domain.test.dto.request.TestInfoRequestDto;
 import com.project.mapdagu.utils.TestUserArgumentResolver;
@@ -13,14 +16,22 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -60,6 +71,24 @@ class EvaluationControllerTest {
     }
 
     @Test
+    void 맵기_평가_수정() throws Exception {
+        //given
+        Long evaluationId = 1L;
+        EvaluationUpdateRequestDto requestDto = new EvaluationUpdateRequestDto(3);
+
+        //when
+        ResultActions result = mockMvc.perform(
+                patch("/api/evaluations/{evaluationId}", evaluationId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDto))
+        );
+
+        //then
+        result.andExpect(status().isNoContent());
+        verify(evaluationService, times(1)).updateEvaluation(anyString(), any(), anyLong());
+    }
+
+    @Test
     void 맵기_평가_이후_정보_저장() throws Exception {
         //given
         EvaluationInfoRequestDto requestDto = new EvaluationInfoRequestDto(1234,3);
@@ -80,7 +109,7 @@ class EvaluationControllerTest {
     void 맵기_평가_하나_조회() throws Exception {
         //given
         Long evaluationId = 1L;
-        EvaluationGetResponseDto responseDto = new EvaluationGetResponseDto("신라면", 1);
+        EvaluationGetResponseDto responseDto = new EvaluationGetResponseDto("신라면", 1, 2);
 
         //when
         when(evaluationService.getOneEvaluation(anyString(), anyLong())).thenReturn(responseDto);
@@ -92,4 +121,26 @@ class EvaluationControllerTest {
         result.andExpect(status().isOk());
         verify(evaluationService, times(1)).getOneEvaluation(anyString(), anyLong());
     }
+
+//    @Test
+//    void 맵기_평가_목록_조회() throws Exception {
+//        //given
+//        Pageable pageable = PageRequest.of(0, 10);
+//        List<EvaluationsGetResponseDto> dtos = new ArrayList<>();
+//        dtos.add(new EvaluationsGetResponseDto("신라면", 1, 3));
+//        dtos.add(new EvaluationsGetResponseDto("진라면", 2, 2));
+//        Page<EvaluationsGetResponseDto> pageResult = new PageImpl<EvaluationsGetResponseDto>(dtos, pageable, 2);
+//
+//        //when
+////        when(evaluationService.getEvaluations(anyString(), any(Pageable.class)).getContent().
+//        ResultActions result = mockMvc.perform(
+//                get("/api/evaluations")
+//                        .param("page", "0")
+//                        .param("size", "10")
+//        );
+//
+//        //then
+//        result.andExpect(status().isOk());
+//        verify(evaluationService, times(1)).getEvaluations(anyString(), any(Pageable.class));
+//    }
 }
