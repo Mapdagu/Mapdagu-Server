361836e6943d6ad58bb2a35531a9078d5109247b
kyeong-hyeok <minkh4227@naver.com>
2023-09-01 16:48:47 +0900

    test: 음식 검색 테스트 코드 추가 (#35)

---
+++ src/test/java/com/project/mapdagu/domain/food/controller/FoodControllerTest.java
@@ -0,0 +1,71 @@
+package com.project.mapdagu.domain.food.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.mapdagu.domain.food.dto.response.FoodSearchResponseDto;
+import com.project.mapdagu.domain.food.service.FoodService;
+import com.project.mapdagu.utils.TestUserArgumentResolver;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class FoodControllerTest {
+
+    @InjectMocks
+    private FoodController foodController;
+    @Mock
+    private FoodService foodService;
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(foodController)
+                .setCustomArgumentResolvers(new TestUserArgumentResolver(), new PageableHandlerMethodArgumentResolver())
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .build();
+    }
+
+    @Test
+    void 음식_검색() throws Exception {
+        //given
+        PageRequest pageable = PageRequest.of(0, 2);
+        List<FoodSearchResponseDto> dtos = new ArrayList<>();
+        dtos.add(new FoodSearchResponseDto("신라면", 1, 3400));
+        dtos.add(new FoodSearchResponseDto("진라면", 2, 1270));
+        Slice<FoodSearchResponseDto> response = new SliceImpl<>(dtos, pageable, false);
+
+        //when
+        given(foodService.searchFood(anyString(), anyString(), any())).willReturn(response);
+        ResultActions result = mockMvc.perform(
+                get("/api/food")
+                        .param("search", "라면")
+        );
+
+        //then
+        result.andExpect(status().isOk());
+        verify(foodService, times(1)).searchFood(anyString(), anyString(), any());
+    }
+}
