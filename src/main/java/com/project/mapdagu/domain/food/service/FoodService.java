ae3f76abfa54948aa3e42ea3b1a798a1b35a04b1
kyeong-hyeok <minkh4227@naver.com>
2023-09-05 13:52:37 +0900

    feat: 음식 스코빌 지수 검색 로직 구현 (#38)

--- src/main/java/com/project/mapdagu/domain/food/service/FoodService.java
+++ src/main/java/com/project/mapdagu/domain/food/service/FoodService.java
@@ -1,8 +1,11 @@
 package com.project.mapdagu.domain.food.service;
 
+import com.project.mapdagu.domain.food.dto.response.FoodScovilleSearchResponseDto;
 import com.project.mapdagu.domain.food.dto.response.FoodSearchResponseDto;
 import com.project.mapdagu.domain.food.entity.Food;
 import com.project.mapdagu.domain.food.repository.FoodRepository;
+import com.project.mapdagu.error.ErrorCode;
+import com.project.mapdagu.error.exception.custom.BusinessException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
@@ -21,6 +24,12 @@ public class FoodService {
     public Slice<FoodSearchResponseDto> searchFood(String email, String search, Pageable pageable) {
         Slice<Food> result = foodRepository.findByNameContaining(search);
         Slice<FoodSearchResponseDto> response = result.map(f -> FoodSearchResponseDto.from(f));
+        return response;
+    }
+
+    public FoodScovilleSearchResponseDto searchFoodScoville(String email, String search) {
+        Food food = foodRepository.findByName(search).orElseThrow(() -> new BusinessException(ErrorCode.FOOD_NOT_FOUND));
+        FoodScovilleSearchResponseDto response = FoodScovilleSearchResponseDto.from(food);
         return response;
     }
 }
