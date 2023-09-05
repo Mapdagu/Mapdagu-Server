1a2f30ae22fe3f1776584f8cfc2cc2874a58ba09
kyeong-hyeok <minkh4227@naver.com>
2023-09-05 16:13:33 +0900

    feat: EvaluationsGetResponseDto에 id 추가 (#39)

--- src/main/java/com/project/mapdagu/domain/evaluation/dto/response/EvaluationsGetResponseDto.java
+++ src/main/java/com/project/mapdagu/domain/evaluation/dto/response/EvaluationsGetResponseDto.java
@@ -2,9 +2,9 @@ package com.project.mapdagu.domain.evaluation.dto.response;
 
 import com.project.mapdagu.domain.evaluation.entity.Evaluation;
 
-public record EvaluationsGetResponseDto(String name, Integer imageNum, Integer score) {
+public record EvaluationsGetResponseDto(Long id, String name, Integer imageNum, Integer score) {
 
     public static EvaluationsGetResponseDto from(Evaluation evaluation) {
-        return new EvaluationsGetResponseDto(evaluation.getFood().getName(), evaluation.getFood().getImageNum(), evaluation.getScore());
+        return new EvaluationsGetResponseDto(evaluation.getId(), evaluation.getFood().getName(), evaluation.getFood().getImageNum(), evaluation.getScore());
     }
 }
