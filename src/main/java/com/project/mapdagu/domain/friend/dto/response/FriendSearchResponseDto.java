a70f71fd80a3cf7512f0b2185d69d13c71896175
kyeong-hyeok <minkh4227@naver.com>
2023-09-07 13:56:08 +0900

    feat: 친구 - 회원 이름 검색 Controller, ResponseDto 추가 (#40)

---
+++ src/main/java/com/project/mapdagu/domain/friend/dto/response/FriendSearchResponseDto.java
@@ -0,0 +1,10 @@
+package com.project.mapdagu.domain.friend.dto.response;
+
+import com.project.mapdagu.domain.member.entity.Member;
+
+public record FriendSearchResponseDto(Long id, Integer imageNum, String username, Integer level) {
+
+    public static FriendSearchResponseDto from(Member member){
+        return new FriendSearchResponseDto(member.getId(), member.getImageNum(), member.getUserName(), member.getLevel());
+    }
+}
---
+++ src/main/java/com/project/mapdagu/domain/friend/entity/Friend.java
@@ -0,0 +1,29 @@
+package com.project.mapdagu.domain.friend.entity;
+
+import com.project.mapdagu.common.entity.BaseTimeEntity;
+import com.project.mapdagu.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+public class Friend extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memberId")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "friendId")
+    private Member friend;
+
+}
