65cd83235b68408e6f0cef6736ac00f1f8bac1c0
kyeong-hyeok <minkh4227@naver.com>
2023-09-27 20:28:58 +0900

    refactor: FriendSearchResponseDto 필드명 수정 (#50)

--- src/main/java/com/project/mapdagu/domain/friend/dto/response/FriendSearchResponseDto.java
+++ src/main/java/com/project/mapdagu/domain/friend/dto/response/FriendSearchResponseDto.java
@@ -2,7 +2,7 @@ package com.project.mapdagu.domain.friend.dto.response;
 
 import com.project.mapdagu.domain.member.entity.Member;
 
-public record FriendSearchResponseDto(Long id, Integer imageNum, String username, Integer level) {
+public record FriendSearchResponseDto(Long id, Integer imageNum, String userName, Integer level) {
 
     public static FriendSearchResponseDto from(Member member){
         return new FriendSearchResponseDto(member.getId(), member.getImageNum(), member.getUserName(), member.getLevel());
