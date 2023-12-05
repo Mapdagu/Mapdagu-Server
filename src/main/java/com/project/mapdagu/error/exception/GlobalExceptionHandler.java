4646dbb1482ed3b39995da003a547ea0d1d2ec31
kyeong-hyeok <minkh4227@naver.com>
2023-12-06 00:04:07 +0900

    feat: Validation 처리를 위한 에러 handle 추가 (#62)

--- src/main/java/com/project/mapdagu/error/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/mapdagu/error/exception/GlobalExceptionHandler.java
@@ -5,8 +5,15 @@ import com.project.mapdagu.error.exception.custom.BusinessException;
 import com.project.mapdagu.error.exception.custom.TokenException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Slf4j
 @RestControllerAdvice
@@ -30,4 +37,15 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(e.getCode()).body(ErrorResponse.of(e.getCode(), e.getMessage()));
     }
 
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        // 에러 반환이 여러 개일 경우 첫 번째 에러를 반환
+        String firstErrorMessage = bindingResult.getFieldErrors().get(0).getDefaultMessage();
+        // 모든 에러 메시지는 로그로 남김
+        List<String> errorList = bindingResult.getFieldErrors().stream().map(err -> err.getDefaultMessage()).collect(Collectors.toList());
+        log.warn("MethodArgumentNotValidExceptionException = {}", errorList);
+
+        return ResponseEntity.status(BAD_REQUEST).body(ErrorResponse.of(e.getStatusCode().value(), firstErrorMessage));
+    }
 }
